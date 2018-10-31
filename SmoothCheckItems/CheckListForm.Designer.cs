35b9f947645272db9f0e65be2ece90f789bd82b8
MrGOStepz <MrGOStepz@gmail.com>
2018-10-31 18:18:03 +1100

    Check List Items Project

---
+++ SmoothCheckItems/CheckListForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace SmoothCheckItems
+{
+    partial class CheckListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.lbTimeWait1 = new System.Windows.Forms.Label();
+            this.lbTable1 = new System.Windows.Forms.Label();
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.btnDone1 = new System.Windows.Forms.Button();
+            this.gb2 = new System.Windows.Forms.GroupBox();
+            this.btnDone2 = new System.Windows.Forms.Button();
+            this.lv2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gb3 = new System.Windows.Forms.GroupBox();
+            this.btnDone3 = new System.Windows.Forms.Button();
+            this.lv3 = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gb4 = new System.Windows.Forms.GroupBox();
+            this.btnDone4 = new System.Windows.Forms.Button();
+            this.lv4 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.gb1.SuspendLayout();
+            this.gb2.SuspendLayout();
+            this.gb3.SuspendLayout();
+            this.gb4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.btnDone1);
+            this.gb1.Controls.Add(this.lv1);
+            this.gb1.Controls.Add(this.lbTable1);
+            this.gb1.Controls.Add(this.lbTimeWait1);
+            this.gb1.Location = new System.Drawing.Point(75, 14);
+            this.gb1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb1.Name = "gb1";
+            this.gb1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb1.Size = new System.Drawing.Size(295, 701);
+            this.gb1.TabIndex = 0;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "1";
+            // 
+            // lbTimeWait1
+            // 
+            this.lbTimeWait1.AutoSize = true;
+            this.lbTimeWait1.Location = new System.Drawing.Point(101, 24);
+            this.lbTimeWait1.Name = "lbTimeWait1";
+            this.lbTimeWait1.Size = new System.Drawing.Size(75, 20);
+            this.lbTimeWait1.TabIndex = 0;
+            this.lbTimeWait1.Text = "WaitTime";
+            // 
+            // lbTable1
+            // 
+            this.lbTable1.AutoSize = true;
+            this.lbTable1.Location = new System.Drawing.Point(101, 54);
+            this.lbTable1.Name = "lbTable1";
+            this.lbTable1.Size = new System.Drawing.Size(48, 20);
+            this.lbTable1.TabIndex = 1;
+            this.lbTable1.Text = "Table";
+            // 
+            // lv1
+            // 
+            this.lv1.Location = new System.Drawing.Point(7, 89);
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(278, 548);
+            this.lv1.TabIndex = 2;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnDone1
+            // 
+            this.btnDone1.Location = new System.Drawing.Point(7, 643);
+            this.btnDone1.Name = "btnDone1";
+            this.btnDone1.Size = new System.Drawing.Size(278, 50);
+            this.btnDone1.TabIndex = 3;
+            this.btnDone1.Text = "Done";
+            this.btnDone1.UseVisualStyleBackColor = true;
+            // 
+            // gb2
+            // 
+            this.gb2.Controls.Add(this.btnDone2);
+            this.gb2.Controls.Add(this.lv2);
+            this.gb2.Controls.Add(this.label1);
+            this.gb2.Controls.Add(this.label2);
+            this.gb2.Location = new System.Drawing.Point(378, 14);
+            this.gb2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb2.Name = "gb2";
+            this.gb2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb2.Size = new System.Drawing.Size(295, 701);
+            this.gb2.TabIndex = 4;
+            this.gb2.TabStop = false;
+            this.gb2.Text = "1";
+            // 
+            // btnDone2
+            // 
+            this.btnDone2.Location = new System.Drawing.Point(7, 643);
+            this.btnDone2.Name = "btnDone2";
+            this.btnDone2.Size = new System.Drawing.Size(278, 50);
+            this.btnDone2.TabIndex = 3;
+            this.btnDone2.Text = "Done";
+            this.btnDone2.UseVisualStyleBackColor = true;
+            // 
+            // lv2
+            // 
+            this.lv2.Location = new System.Drawing.Point(7, 89);
+            this.lv2.Name = "lv2";
+            this.lv2.Size = new System.Drawing.Size(278, 548);
+            this.lv2.TabIndex = 2;
+            this.lv2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Table";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "WaitTime";
+            // 
+            // gb3
+            // 
+            this.gb3.Controls.Add(this.btnDone3);
+            this.gb3.Controls.Add(this.lv3);
+            this.gb3.Controls.Add(this.label3);
+            this.gb3.Controls.Add(this.label4);
+            this.gb3.Location = new System.Drawing.Point(681, 14);
+            this.gb3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb3.Name = "gb3";
+            this.gb3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb3.Size = new System.Drawing.Size(295, 701);
+            this.gb3.TabIndex = 5;
+            this.gb3.TabStop = false;
+            this.gb3.Text = "1";
+            // 
+            // btnDone3
+            // 
+            this.btnDone3.Location = new System.Drawing.Point(7, 643);
+            this.btnDone3.Name = "btnDone3";
+            this.btnDone3.Size = new System.Drawing.Size(278, 50);
+            this.btnDone3.TabIndex = 3;
+            this.btnDone3.Text = "Done";
+            this.btnDone3.UseVisualStyleBackColor = true;
+            // 
+            // lv3
+            // 
+            this.lv3.Location = new System.Drawing.Point(7, 89);
+            this.lv3.Name = "lv3";
+            this.lv3.Size = new System.Drawing.Size(278, 548);
+            this.lv3.TabIndex = 2;
+            this.lv3.UseCompatibleStateImageBehavior = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Table";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "WaitTime";
+            // 
+            // gb4
+            // 
+            this.gb4.Controls.Add(this.btnDone4);
+            this.gb4.Controls.Add(this.lv4);
+            this.gb4.Controls.Add(this.label5);
+            this.gb4.Controls.Add(this.label6);
+            this.gb4.Location = new System.Drawing.Point(985, 14);
+            this.gb4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb4.Name = "gb4";
+            this.gb4.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gb4.Size = new System.Drawing.Size(295, 701);
+            this.gb4.TabIndex = 4;
+            this.gb4.TabStop = false;
+            this.gb4.Text = "1";
+            // 
+            // btnDone4
+            // 
+            this.btnDone4.Location = new System.Drawing.Point(7, 643);
+            this.btnDone4.Name = "btnDone4";
+            this.btnDone4.Size = new System.Drawing.Size(278, 50);
+            this.btnDone4.TabIndex = 3;
+            this.btnDone4.Text = "Done";
+            this.btnDone4.UseVisualStyleBackColor = true;
+            // 
+            // lv4
+            // 
+            this.lv4.Location = new System.Drawing.Point(7, 89);
+            this.lv4.Name = "lv4";
+            this.lv4.Size = new System.Drawing.Size(278, 548);
+            this.lv4.TabIndex = 2;
+            this.lv4.UseCompatibleStateImageBehavior = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(101, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Table";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(101, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "WaitTime";
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(12, 233);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(55, 174);
+            this.btnLeft.TabIndex = 6;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(1287, 233);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(55, 174);
+            this.btnRight.TabIndex = 7;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // CheckListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.gb4);
+            this.Controls.Add(this.gb3);
+            this.Controls.Add(this.gb2);
+            this.Controls.Add(this.gb1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CheckListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Smooth Check List";
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.gb2.ResumeLayout(false);
+            this.gb2.PerformLayout();
+            this.gb3.ResumeLayout(false);
+            this.gb3.PerformLayout();
+            this.gb4.ResumeLayout(false);
+            this.gb4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.Button btnDone1;
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.Label lbTable1;
+        private System.Windows.Forms.Label lbTimeWait1;
+        private System.Windows.Forms.GroupBox gb2;
+        private System.Windows.Forms.Button btnDone2;
+        private System.Windows.Forms.ListView lv2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gb3;
+        private System.Windows.Forms.Button btnDone3;
+        private System.Windows.Forms.ListView lv3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox gb4;
+        private System.Windows.Forms.Button btnDone4;
+        private System.Windows.Forms.ListView lv4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+    }
+}
+
---
+++ SmoothCheckItems/ItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothCheckItems
+{
+    public class ItemModel
+    {
+        public int TableNo { get; set; }
+        public DateTime CallTime { get; set; }
+        public List<ItemDetail> ListEntreeItems = new List<ItemDetail>();
+        public List<ItemDetail> ListMainItems = new List<ItemDetail>();
+        public List<ItemDetail> ListDessertItems = new List<ItemDetail>();
+        public List<ItemDetail> ListBeverageItems = new List<ItemDetail>();
+    }
+
+    public class ItemDetail
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public TypeOfFood TypeFood { get; set; }
+        public string ImageFood { get; set; }
+        public string ImagePopup { get; set; }
+    }
+
+    public enum TypeOfFood
+    {
+        Entree,
+        Main,
+        Dessert,
+        Beverage
+    }
+}
